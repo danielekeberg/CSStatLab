74fc81bb29fb80dc62101d949853d1f6d413c0b3
Daniel Lehre Ekeberg <danieleke@live.no>
2025-12-13 02:08:25 +0100

    RecentMatches gray text when tie

--- app/components/RecentMatches.tsx
+++ app/components/RecentMatches.tsx
@@ -21,7 +21,7 @@ export default function RecentMatches({ matches }: { matches: any[]}) {
                                 <td>{m.map_name ? <div className="flex items-center gap-2"><img className="h-5" src={`https://leetify.com/assets/images/map-icons/${m.map_name}.svg`}/><p className="hidden md:block">{m.map_name.slice(3,4).toUpperCase()}{m.map_name.slice(4)}</p></div> : <p>{m.map_name ? m.map_name : 'No data'}</p>}</td>
                                 <td className="flex items-center justify-start">{m.has_banned_player ? <p className="px-2 border border-red-800 rounded-xs font-bold text-xs py-0 text-[#eae8e0]" title="A player in this game has been banned">Ban</p> : null}</td>
                                 <td className="hidden md:block">{m.mode === "matchmaking_competitive" ? "Competitive" : "Premier"}</td>
-                                <td className="font-bold text-center">{m.rounds_won > m.rounds_lost ? <p className="text-green-500">{m.rounds_won}:{m.rounds_lost}</p> : <p className="text-red-500">{m.rounds_lost}:{m.rounds_won}</p>}</td>
+                                <td className={`font-bold text-center ${m.rounds_won > m.rounds_lost ? 'text-green-500' : ''} ${m.rounds_won < m.rounds_lost ? 'text-red-500' : ''} ${m.rounds_won === m.rounds_lost ? 'text-{#eae8e0]' : ''}}`}>{m.rounds_won > m.rounds_lost ? <p>{m.rounds_won}:{m.rounds_lost}</p> : <p>{m.rounds_lost}:{m.rounds_won}</p>}</td>
                                 <td>{(m.total_kills / m.total_deaths).toFixed(2)}</td>
                                 <td>{m.total_kills}</td>
                                 <td>{m.total_deaths}</td>
