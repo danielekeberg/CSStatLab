3ccd1697c607096ef5b2f326c3a041115fc1bb8e
Daniel Lehre Ekeberg <danieleke@live.no>
2025-12-14 16:54:05 +0100

    Changed how time since match is being displayed

--- app/components/FormatMatchTime.tsx
+++ app/components/FormatMatchTime.tsx
@@ -6,6 +6,7 @@ export function formatMatchTime(iso: string): string {
     const diffSec = Math.floor(diffMs / 1000);
     const diffMin = Math.floor(diffSec / 60);
     const diffHr = Math.floor(diffMin / 60);
+    const diffDays = Math.floor(diffHr / 24)
     if(diffSec < 60) {
         return "Just now";
     }
@@ -15,10 +16,9 @@ export function formatMatchTime(iso: string): string {
     if(diffHr < 24) {
         return `${diffHr} hour${diffHr !== 1 ? "s" : ""} ago`;
     }
+    if(diffDays < 10) {
+        return `${diffDays} day${diffDays !== 1 ? "s" : ""} ago`;
+    }
 
-    return date.toLocaleDateString(undefined, {
-        day: "2-digit",
-        month: "short",
-        year: "numeric",
-    })
+    return date.toLocaleDateString("no-NO")
 }
--- app/components/RecentMatches.tsx
+++ app/components/RecentMatches.tsx
@@ -7,8 +7,8 @@ export default function RecentMatches({ matches }: { matches: any[]}) {
             <div className="w-full p-2 grid grid-cols-7 md:grid-cols-8 text-sm opacity-70 font-bold border-b border-neutral-500 text-center">
                 <p className="text-left">Map</p>
                 <p></p>
-                <p className="text-left hidden md:block">Mode</p>
-                <p className="text-center">Score</p>
+                <p className="text-left">Score</p>
+                <p className="text-center hidden md:block">Mode</p>
                 <p>K/D</p>
                 <p>Kills</p>
                 <p>ADR</p>
@@ -21,8 +21,8 @@ export default function RecentMatches({ matches }: { matches: any[]}) {
                             <tr key={m.match_id} className={`p-2 grid grid-cols-7 md:grid-cols-8 border-b border-neutral-500/20 w-full ${m.has_banned_player && 'bg-red-900/40'}`}>
                                 <td>{m.map_name ? <div className="flex items-center gap-2"><img className="h-5" src={`https://leetify.com/assets/images/map-icons/${m.map_name}.svg`}/><p className="hidden md:block">{m.map_name.slice(3,4).toUpperCase()}{m.map_name.slice(4)}</p></div> : <p>{m.map_name ? m.map_name : 'No data'}</p>}</td>
                                 <td className="flex items-center justify-start">{m.has_banned_player ? <p className="px-2 border border-red-800 rounded-xs font-bold text-xs py-0 text-[#eae8e0]" title="A player in this game has been banned">Ban</p> : null}</td>
-                                <td className="text-left hidden md:block">{m.mode === "matchmaking_competitive" && "Competitive"}{m.mode === "faceit" && "FACEIT"}{m.mode === "matchmaking" && "Premier"}{m.mode === "hltv" && "HLTV"}</td>
-                                <td className={`font-bold text-center ${m.rounds_won > m.rounds_lost && 'text-green-500'} ${m.rounds_won < m.rounds_lost && 'text-red-500'} ${m.rounds_won === m.rounds_lost && 'text-{#eae8e0]'}}`}>{m.rounds_won > m.rounds_lost ? <p>{m.rounds_won}:{m.rounds_lost}</p> : <p>{m.rounds_lost}:{m.rounds_won}</p>}</td>
+                                <td className={`font-bold text-left ${m.rounds_won > m.rounds_lost && 'text-green-500'} ${m.rounds_won < m.rounds_lost && 'text-red-500'} ${m.rounds_won === m.rounds_lost && 'text-{#eae8e0]'}}`}>{m.rounds_won > m.rounds_lost ? <p>{m.rounds_won}:{m.rounds_lost}</p> : <p>{m.rounds_lost}:{m.rounds_won}</p>}</td>
+                                <td className="text-center hidden md:block">{m.mode === "matchmaking_competitive" && "Competitive"}{m.mode === "faceit" && "FACEIT"}{m.mode === "matchmaking" && "Premier"}{m.mode === "hltv" && "HLTV"}</td>
                                 <td>{(m.total_kills / m.total_deaths).toFixed(2)}</td>
                                 <td>{m.total_kills}</td>
                                 <td>{m.dpr.toFixed(0)}</td>
