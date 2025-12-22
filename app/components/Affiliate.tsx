5c7ec003ee49b0694e3a77e545b844dacaf21c05
Daniel Lehre Ekeberg <danieleke@live.no>
2025-12-22 01:25:58 +0100

    Update

--- app/components/Affiliate.tsx
+++ app/components/Affiliate.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 export default function Affiliate() {
     return(
         <Link target="_blank" href="https://csgoroll.com/r/daniru">
-            <div className="my-10 p-5 bg-[#383838]/20 border border-[#383838] rounded-xl text-center">
+            <div className="my-10 p-5 bg-[#383838]/20 cursor-default border border-[#383838] rounded-xl text-center">
                 <h1 className="text-xl font-bold">🎲 CSGOROLL bonus</h1>
                 <p className="text-[#b3c0d3]">Use code <strong>DANIRU</strong> to get free coins on CSGOROLL</p>
                 <button className="p-2 border bg-[#383838]/20 border-[#383838] rounded mt-3 hover:bg-black/50 cursor-pointer transition duration-200 font-bold">Get free coins</button>
