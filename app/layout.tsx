cd5ab0564659d0d557bf708265267cf999416a27
Daniel Lehre Ekeberg <danieleke@live.no>
2026-01-09 23:23:50 +0100

    AdSense

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,30 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { DM_Sans } from "next/font/google";
-import { Analytics } from "@vercel/analytics/next"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import { DM_Sans } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const dmSans = DM_Sans({
-  subsets: ["latin"],
-  weight: ["400", "500", "700"],
-  variable: "--font-dm-sans",
-})
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+  variable: '--font-dm-sans',
+});
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "CSStatLab.com",
-  description: "Analyze any CS2 player with real match data. View KD, win rate, pre-aim, reaction time, accuracy and more. Fast syncing, clean stats and smart cheat-risk detection.",
+  title: 'CSStatLab.com',
+  description:
+    'Analyze any CS2 player with real match data. View KD, win rate, pre-aim, reaction time, accuracy and more. Fast syncing, clean stats and smart cheat-risk detection.',
 };
 
 export default function RootLayout({
@@ -33,13 +34,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7255204765091044"
+        crossOrigin="anonymous"
+      ></script>
       <Analytics />
       <SpeedInsights />
-      <body
-        className={`${dmSans.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${dmSans.variable} antialiased`}>{children}</body>
     </html>
   );
 }
